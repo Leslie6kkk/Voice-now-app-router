b51ad4f559927db20598127482c8fa1a9d603842
Xiaoke Li <x2354li@uwaterloo.ca>
2024-09-17 22:36:06 -0400

    configured postgres and prisma

---
+++ prisma/user.ts
@@ -0,0 +1,22 @@
+import prisma from "@/lib/prisma";
+
+; // Assuming you have your Prisma client setup
+
+// Find user by email
+export const findUserByEmail = async (email: string) => {
+  return await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+};
+
+// Create a new user
+export const createUser = async (email: string, passwordHash: string) => {
+  return await prisma.user.create({
+    data: {
+      email,
+      password: passwordHash, // Store hashed password
+    },
+  });
+};
